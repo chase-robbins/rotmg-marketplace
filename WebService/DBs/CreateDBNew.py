7890c4e70442dc6d53e7b68df027adabfc90774a
Chase Robbins <chaserobbins@me.com>
2020-05-14 17:39:24 -0700

    Added InitiateTransaction and can now query for mules given item

--- WebService/DBs/CreateDBNew.py
+++ WebService/DBs/CreateDBNew.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+import InitiateTransaction
 meta = MetaData()
 
 #create engine for SQLAlchemy ORM
-engine = create_engine('postgresql://postgres:testingPassword@localhost/postgres', echo = True)
+engine = create_engine('postgresql://postgres:testingPassword@localhost/postgres')
 
 items = Table(
     'items', meta,
@@ -73,21 +74,37 @@ users = Table(
     Column('RegistrationDate', Integer),
 )
 
+#Table has been created. Will leave code above for reference. Note sure what the correct standards are for handling database itililization code.
+#meta.create_all(engine)
 
-meta.create_all(engine)
-ins = items.insert(), [
-    {'name' : 'Potion of Life', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Defence', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Dexterity', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Wisdom', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Life', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214}, ]
-
+# This block of code will insert these items into the database in dictionary form.
+#
+# conn = engine.connect()
+# result = conn.execute(items.insert(), [
+#     {'name' : 'Potion of Life', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
+#     {'name' : 'Potion of Defence', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
+#     {'name' : 'Potion of Dexterity', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
+#     {'name' : 'Potion of Wisdom', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
+#     {'name' : 'Potion of Life', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
+#     ])
 
 conn = engine.connect()
-result = conn.execute(items.insert(), [
-    {'name' : 'Potion of Life', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Defence', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Dexterity', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Wisdom', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    {'name' : 'Potion of Life', 'MuleID' : 123, 'GameID' : 1234, 'Owner' : 214},
-    ])
+s = items.select()
+result = conn.execute(s)
+
+row = result.fetchone()
+
+for row in result:
+    print (row)
+
+# Script to search for life pots
+# print("Searching through database to locate life pots...")
+# s = items.select().where(items.c.name == "Potion of Life")
+# result = conn.execute(s)
+#
+# for row in result:
+#     print (row)
+
+#TransactionArray = ["Hello world.", "Dog"]
+
+InitiateTransaction.main("Chase", "Test", 4, items)
---
+++ WebService/DBs/InitiateTransaction.py
@@ -0,0 +1,29 @@
+from sqlalchemy import select, create_engine, MetaData, Table, Column, Integer, String
+
+
+#Connect to DB (next 2)
+engine = create_engine('postgresql://postgres:testingPassword@localhost/postgres')
+conn = engine.connect()
+
+#Method to find the mule that is holding item in question
+def findMule(ownerID, itemID, items):
+    #searches for item matching itemID inside items table
+    s = items.select().where(items.c.id == itemID)
+    result = conn.execute(s)
+    row = result.fetchone()
+    nameOfItem = row[1]
+    muleID = row[2]
+    print("Mule located. Item " + nameOfItem + " is located on mule with ID " + str(muleID) )
+    return muleID
+
+#Pass in arguments in order: Recipient User ID, Recipient to Provide, Item (ID) to Give
+def main(recipient, providing, toGive, items):
+    print("Recipiet " + recipient + " will recieve " + str(toGive) + " via mule " + str(findMule(3, toGive, items)) + " in exchange for " + providing)
+    findMule(3, toGive, items)
+
+    #Final insertion into transactions ledger
+    #insertion = transactions.insert().values(UserID = recepient, MuleID = mule )
+
+
+if __name__ == '__main__':
+    main()
