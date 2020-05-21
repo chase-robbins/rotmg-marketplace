47bd3315778946cfaf825f1fb7c061d50421ef52
Chase Robbins <chaserobbins@me.com>
2020-05-21 09:39:45 -0700

    reorganized project, flask progress

--- WebService/main.py
+++ WebService/main.py
@@ -2,10 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
+@app.route("/")
 def home():
-    return render_template("index.html", content="")
-
+    return render_template("home.html", content="home.html")
 
 
 if __name__ == "__main__":
