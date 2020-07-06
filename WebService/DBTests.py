2c76ac726c0e67b265040c59d4810ceb87916100
Chase Robbins <chaserobbins@me.com>
2020-07-06 16:35:36 -0700

    heroku deployment changes and file restructuring

--- WebService/DBTests.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-
-engine = create_engine('postgresql://postgres:testingPassword@localhost/postgres', echo = True)
-meta = MetaData()
-
-ins = items.insert().values(name = 'Potion of Life', MuleID = 123, GameID = 1234, Owner = 214)
-conn = engine.connect()
-result = conn.execute(ins)
--- WebService/dropAllTables.py
+++
@@ -1,3 +0,0 @@
-import DBManager
-
-DBManager.DropAll()
